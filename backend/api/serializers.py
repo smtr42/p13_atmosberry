868f5319dc3ba2b2c14faea61433ca111db17560
Teiva <simteiva@gmail.com>
2020-12-02 19:26:29 +0100

    Add front page and map

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,15 +3,6 @@ from rest_framework import serializers
 from .models import Address, Device, Reading, Sensor
 
 
-class SensorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sensor
-        fields = (
-            "sensor_type",
-            "name",
-        )
-
-
 class ReadingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reading
@@ -21,6 +12,24 @@ class ReadingSerializer(serializers.ModelSerializer):
             "sensor",
         )
         depth = 2
+
+class SensorSerializer(serializers.ModelSerializer):
+    reading = ReadingSerializer(many=True)
+    class Meta:
+        model = Sensor
+        fields = (
+            "sensor_type",
+            "name",
+            "sensor_reading",
+        )
+
+    def create(self, validated_data):
+        readings_data = validated_data.pop('data')
+        sensor = Sensor.objects.create(**validated_data)
+        for readings in readings_data:
+            Reading.objects.create(sensor=sensor, **readings)
+        return sensor
+
 
 
 class AddressSerializer(serializers.ModelSerializer):
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 
 from .models import Reading, Sensor
-from .serializers import ReadingSerializer
+from .serializers import ReadingSerializer, SensorSerializer
 
 
 class ReadingView(generics.ListCreateAPIView):
@@ -24,3 +24,30 @@ class ReadingView(generics.ListCreateAPIView):
         new_reading.save()
         serializer = ReadingSerializer(new_reading)
         return Response(serializer.data)
+
+
+class SensorView(generics.ListCreateAPIView):
+    serializer_class = SensorSerializer
+
+    def get_queryset(self):
+        get_sensor = Sensor.objects.all()
+        return get_sensor
+
+    def create(self, request, *args, **kwargs):
+        reading_data = request.data
+
+        new_reading = Reading.objects.create(
+            sensor=Sensor.objects.get(name=reading_data["sensor"]),
+            data=reading_data["data"],
+            timestamp=reading_data["timestamp"],
+        )
+        new_reading.save()
+        serializer = ReadingSerializer(new_reading)
+        return Response(serializer.data)
+    
+    def create(self, validated_data):
+        tracks_data = validated_data.pop('data')
+        album = Album.objects.create(**validated_data)
+        for track_data in tracks_data:
+            Track.objects.create(album=album, **track_data)
+        return album
